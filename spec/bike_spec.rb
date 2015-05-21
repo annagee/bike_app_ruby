06ab403a04fdd57fa74e65a4c1bac765d0bf5e38
Anna Gaughan <agaughan@gmail.com>
2015-05-21 16:06:02 +0100

    first test passed

---
+++ spec/bike_spec.rb
@@ -0,0 +1,16 @@
+require_relative  '../lib/bike'
+
+# We're describing the functionality of a specific class, Bike
+describe  Bike  do
+
+  # this is specific feature or behaviour
+  # that we EXPECT to have
+  it 'should not be broken when created' do 
+    bike = Bike.new 
+    # Instantiate a new instance of the Bike class
+    expect(bike.broken?).to be false
+    # expect (bike.broken?).not_to be ture
+      
+  end
+
+end
